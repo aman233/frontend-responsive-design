a4d91fd7c0a351f8a668cf297f75f8dade359f50
Aman Seddiqi <Aman233@hotmail.nl>
2020-01-24 14:53:34 +0100

    header en nav
    haha eindelijk de rest van de website

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,43 @@
-var hamburger = document.getElementById("menu");
+var nav = document.querySelector("nav")
+var header = document.querySelector("header")
+var article = document.querySelector("article")
+var body = document.querySelector("body")
+var section = document.querySelector("section")
+var main = document.querySelector("main")
+var form = document.querySelector("form")
+var hamburger = document.getElementById("menu")
+var closenav = document.getElementById("cross")
+var logo = document.getElementById("logo-head")
+var title = document.getElementById("logo-title")
+
 
 hamburger.addEventListener("click", function() {
-    element.class
-})
+    nav.classList.add("visible")
+    hamburger.classList.add("gone")
+    logo.classList.add("gone")
+    title.classList.add("gone")
+    article.classList.add("gone")
+    section.classList.add("gone")
+    main.classList.add("gone")
+    form.classList.add("gone")
+    header.classList.add("header-big")
+    body.classList.add("scrolling")
+});
+
+closenav.addEventListener("click", function() {
+    nav.classList.remove("visible")
+    hamburger.classList.remove("gone")
+    logo.classList.remove("gone")
+    title.classList.remove("gone")
+    article.classList.remove("gone")
+    section.classList.remove("gone")
+    main.classList.remove("gone")
+    form.classList.remove("gone")
+    header.classList.remove("header-big")
+    body.classList.remove("scrolling")
+
+});
+
+
+
+// Add event listener to hamburger menu to open overlay
