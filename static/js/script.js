d343fb4e4d1b178af707974417f488210143ad7e
aman233 <Aman233@hotmail.nl>
2020-01-26 05:26:02 +0100

    Big things
    js met header scroll
    css update met kleurtjes en ander onzin
    html article grid

--- static/js/script.js
+++ static/js/script.js
@@ -10,7 +10,6 @@ var closenav = document.getElementById("cross")
 var logo = document.getElementById("logo-head")
 var title = document.getElementById("logo-title")
 
-
 hamburger.addEventListener("click", function() {
     nav.classList.add("visible")
     hamburger.classList.add("gone")
@@ -39,5 +38,24 @@ closenav.addEventListener("click", function() {
 });
 
 
+// Broertje hielp met uitleg
+var headerHeight = header.offsetHeight; //headheight kennen
+var latestScroll = 0;
+var previousScroll = 0; //prevsroll begint op 0
+window.onscroll = function() { //bekijkt wanneer de gebruiker scrollt
+    latestScroll = window.pageYOffset; // Bekijkt waar de gebruiker is op de pagina -> Y graden
 
-// Add event listener to hamburger menu to open overlay
+    if (latestScroll > headerHeight) { // kijkt of latestscroll groter is dan headerheight -> dus of de gebruiker langs de header is, die 97 px
+        if (previousScroll > latestScroll) { // kijkt of gebruiker naar boven of naar beneden scrolled
+            header.classList.add('fixed'); //voegt header toe
+            section.classList.add("fixed-padding"); // voegt padding zodat er geen verspringing is
+            header.classList.remove('hidden'); //haalt de display:none weg
+        } else {
+            header.classList.add('hidden'); //scroll naar beneden voogt display:none toe
+        }
+    } else { //als de gebruiker naar de top van de pagina is verdwijnt dit effect
+        header.classList.remove('fixed');
+        section.classList.remove("fixed-padding")
+    }
+    previousScroll = latestScroll; //slaat de scroll positie van de gebruiker op voor een nieuwe check om header te tonen of niet
+};
